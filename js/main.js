bb2f7887ca3b5ad99f550b0ca992ff1a15531b06
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-09 09:32:43 +0800

    Don't use 'this'

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 const content = document.getElementById('content');
 
-document.getElementsByClassName('text-alignment')[0].addEventListener( 'click', function() {
+document.getElementsByClassName('text-alignment')[0].addEventListener( 'click', function(e) {
 	content.className = '';
 
 	var alignClass = 'mid';
-	if( this.getAttribute('data-align') == 'right' ) {
+	if( e.currentTarget.getAttribute('data-align') == 'right' ) {
 		alignClass = 'left';
 	}
-	else if( this.getAttribute('data-align') == 'mid' ) {
+	else if( e.currentTarget.getAttribute('data-align') == 'mid' ) {
 		alignClass = 'right';
 	}
-	this.setAttribute('data-align', alignClass);
-	this.className = `text-alignment ${alignClass}`;
+	e.currentTarget.setAttribute('data-align', alignClass);
+	e.currentTarget.className = `text-alignment ${alignClass}`;
 	content.className = alignClass;
 });
 
-document.getElementsByClassName('text-color')[0].addEventListener( 'click', function() {
-	if( this.getAttribute('data-color') == 'white' ) {
-		this.setAttribute('data-color', 'black');
-		this.className = 'text-color';
+document.getElementsByClassName('text-color')[0].addEventListener( 'click', function(e) {
+	if( e.currentTarget.getAttribute('data-color') == 'white' ) {
+		e.currentTarget.setAttribute('data-color', 'black');
+		e.currentTarget.className = 'text-color';
 		content.className = '';
 	}
 	else {
-		this.setAttribute('data-color', 'white');
-		this.className = 'text-color white';
+		e.currentTarget.setAttribute('data-color', 'white');
+		e.currentTarget.className = 'text-color white';
 		content.className = 'white';
 	}
 });
 
-document.getElementsByClassName('text-size')[0].addEventListener( 'input', function() {
-	content.style.fontSize = `${parseInt(this.value)}px`;
+document.getElementsByClassName('text-size')[0].addEventListener( 'input', function(e) {
+	content.style.fontSize = `${parseInt(e.currentTarget.value)}px`;
 });
 
 var bgColorArray = document.getElementsByClassName('color');
 for( var colorDiv of Array.from(bgColorArray) ) {
-	colorDiv.addEventListener( 'click', function() {
-		document.body.className = `v-mid bg-${this.getAttribute('data-color')}`;
+	colorDiv.addEventListener( 'click', function(e) {
+		document.body.className = `v-mid bg-${e.currentTarget.getAttribute('data-color')}`;
 	});
 }
