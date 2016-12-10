3ea10d66a6df0212eb3404770d30b3a615390c30
EddieWen-Taiwan <m20021991y@gmail.com>
2016-12-10 23:45:13 +0800

    show flash animation by clicking

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
+import './css/animate.css';
 import './css/reset.css';
 import './css/main.css';
 
 const content = document.getElementById('content');
+content.addEventListener( 'click', (e) => {
 
+	e.stopPropagation();
+
+});
+document.getElementsByClassName('control-area')[0].addEventListener( 'click', (e) => {
+
+	e.stopPropagation();
+
+});
+
+/**
+ * alignment mapping
+ */
 const nextAlignMap = {
 	right: 'left',
 	mid: 'right',
 	left: 'mid',
 };
 
+/**
+ * change text alignment
+ */
 document.getElementsByClassName('text-alignment')[0].addEventListener( 'click', (e) => {
 
 	content.className = content.className.replace(
@@ -27,6 +44,9 @@ document.getElementsByClassName('text-alignment')[0].addEventListener( 'click', 
 
 });
 
+/**
+ * change font color
+ */
 document.getElementsByClassName('text-color')[0].addEventListener( 'click', (e) => {
 
 	const setTextColorToBlack = e.currentTarget.getAttribute('data-color') === 'white';
@@ -45,14 +65,19 @@ document.getElementsByClassName('text-color')[0].addEventListener( 'click', (e) 
 
 });
 
+/**
+ * change font size
+ */
 document.getElementsByClassName('text-size')[0].addEventListener( 'input', (e) => {
 
 	content.style.fontSize = `${parseInt(e.currentTarget.value, 10)}px`;
 
 });
 
+/**
+ * setup the menu for changing background color
+ */
 const bgColorArray = document.getElementsByClassName('color');
-
 Array.from(bgColorArray).forEach((color) => {
 
 	color.addEventListener( 'click', (e) => {
@@ -60,5 +85,24 @@ Array.from(bgColorArray).forEach((color) => {
 		document.body.className = `v-mid bg-${e.currentTarget.getAttribute('data-color')}`;
 
 	});
+
+});
+
+/**
+ * make the input flash animation
+ */
+document.body.addEventListener( 'click', () => {
+
+	if( !content.classList.contains('flash') ) {
+
+		content.classList.add('flash');
+
+		setTimeout(() => {
+
+			content.classList.remove('flash');
+
+		}, 700);
+
+	}
 
 });
